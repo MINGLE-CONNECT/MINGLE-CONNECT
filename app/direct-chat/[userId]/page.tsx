ec86b3c317598fb3072209b61e9d8b0b1d613191
MINGLE-CONNECT <davechuks3k6@gmail.com>
2026-09-21 06:40:14 +0100

    Update page.tsx

--- app/direct-chat/[userId]/page.tsx
+++ app/direct-chat/[userId]/page.tsx
@@ -998,7 +998,7 @@ export default function DirectChat() {
   visibility: 'visible',
   opacity: 1,
   width: '44px',
-        width: '44px',
+        
         height: '44px',
         borderRadius: '50%',
         border: 'none',
